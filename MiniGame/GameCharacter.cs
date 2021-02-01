495e7570ba4ba2660d02d9f7af1b7af2047d744a
BradleyMorrison1 <70822639+BradleyMorrison1@users.noreply.github.com>
2021-02-01 14:50:47 -0400

    Removed trail from character

--- MiniGame/GameCharacter.cs
+++ MiniGame/GameCharacter.cs
@@ -14,48 +14,62 @@ namespace MiniGame
         public int playerX = Console.CursorLeft;
         public int playerY = Console.CursorTop;
 
+        public void checkCoordRange()
+        {
+            if (playerX < 0) playerX = 0;
+            if (playerY < 0) playerX = 0;
+        }
+
         public void MovePlayer()
         {
-            
+            checkCoordRange();
 
             ConsoleKeyInfo userInput = Console.ReadKey(true);
 
             switch (userInput.KeyChar)
             {
                 case 'w':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX, playerY--);
                     Console.Write(playerCharacter);
                     break;
                 case 'a':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX--, playerY);
                     Console.Write(playerCharacter);
                     break;
 
                 case 's':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX, playerY++);
                     Console.Write(playerCharacter);
                     break;
 
                 case 'd':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX++, playerY);
                     Console.Write(playerCharacter);
                     break;
 
                 case 'W':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX, playerY--);
                     Console.Write(playerCharacter);
                     break;
                 case 'A':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX--, playerY);
                     Console.Write(playerCharacter);
                     break;
 
                 case 'S':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX, playerY++);
                     Console.Write(playerCharacter);
                     break;
 
                 case 'D':
+                    Console.Clear();
                     Console.SetCursorPosition(playerX++, playerY);
                     Console.Write(playerCharacter);
                     break;
